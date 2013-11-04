b8e4ecbf4b0ed014b335bb18d7f7dc687c542391
Juhamatti Niemelä <iiska@iki.fi>
2013-11-04 18:56:10 +0200

    Cleanup specs

--- spec/utterson_spec.rb
+++ spec/utterson_spec.rb
@@ -34,105 +34,115 @@ describe Utterson do
 
   describe "#check_uri" do
     let(:u) {Utterson.new}
+    let(:html_file) {"file.html"}
 
     it "should use remote checking for http protocol" do
+      url = "http://example.com"
       u.stub(:check_remote_uri) {}
-      u.should_receive(:check_remote_uri).with("http://example.com", "file.html")
-      u.check_uri("http://example.com", "file.html")
+      u.should_receive(:check_remote_uri).with(url, html_file)
+      u.check_uri(url, html_file)
     end
 
     it "should use remote checking for https protocol" do
+      url = "https://example.com"
       u.stub(:check_remote_uri) {}
-      u.should_receive(:check_remote_uri).with("https://example.com", "file.html")
-      u.check_uri("https://example.com", "file.html")
+      u.should_receive(:check_remote_uri).with(url, html_file)
+      u.check_uri(url, html_file)
     end
 
     it "should use remote checking when only // is specified" do
+      url = "//example.com"
       u.stub(:check_remote_uri) {}
-      u.should_receive(:check_remote_uri).with("//example.com", "file.html")
-      u.check_uri("//example.com", "file.html")
+      u.should_receive(:check_remote_uri).with(url, html_file)
+      u.check_uri(url, html_file)
     end
 
     it "should use local checking for relative uris" do
+      url = "../file.html"
       u.stub(:check_local_uri) {}
-      u.should_receive(:check_local_uri).with("../file.html", "file.html")
-      u.check_uri("../file.html", "file.html")
+      u.should_receive(:check_local_uri).with(url, html_file)
+      u.check_uri(url, html_file)
     end
   end
 
   describe "#check_local_uri" do
     let(:u) {Utterson.new(dir: "spec/fixtures/dir-structure")}
+    let(:html_file) {"spec/fixtures/dir-structure/1.htm"}
 
     it "should not assign error info if file exists" do
-      u.check_local_uri("../sample.html", "spec/fixtures/dir-structure/1.htm")
+      u.check_local_uri("../sample.html", html_file)
       u.errors.should be_empty
     end
 
     it "should assign error info if file doesn't exist" do
-      u.check_local_uri("../sample_not_found.html", "spec/fixtures/dir-structure/1.htm")
-      u.errors["spec/fixtures/dir-structure/1.htm"].should == {"../sample_not_found.html" => "File not found"}
+      url = "../sample_not_found.html"
+      u.check_local_uri(url, html_file)
+      u.errors[html_file].should == {url => "File not found"}
     end
 
     it "should use root directory when urls start with /" do
-      u2 = Utterson.new(dir: "spec/fixtures/dir-structure", root: "spec/fixtures")
-      u2.check_local_uri("/sample.html", "spec/fixtures/dir-structure/1.htm")
+      u2 = Utterson.new(dir: "spec/fixtures/dir-structure",
+                        root: "spec/fixtures")
+      u2.check_local_uri("/sample.html", html_file)
       u2.errors.should be_empty
     end
 
     it "should handle target directory as root for urls starting with / if root is no available" do
-      u.check_local_uri("/2.html", "spec/fixtures/dir-structure/1.htm")
+      u.check_local_uri("/2.html", html_file)
       u.errors.should be_empty
     end
 
     it "should ignore query string when checking local files" do
-      u.check_local_uri("2.html?queryparam=value", "spec/fixtures/dir-structure/1.htm")
+      u.check_local_uri("2.html?queryparam=value", html_file)
       u.errors.should be_empty
     end
   end
 
   describe "#check_remote_uri" do
     let(:u) {Utterson.new}
+    let(:html_file) {"test.html"}
+    let(:url) {"http://example.com/index.html"}
 
     it "should not assign error info if request is successfull" do
-      stub_request(:head, "http://example.com/index.html").
+      stub_request(:head, url).
         with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => "", :headers => {})
-      u.check_remote_uri("http://example.com/index.html", "test.html")
+      u.check_remote_uri(url, html_file)
       u.errors.should be_empty
     end
 
     it "should assign error info if there is error response" do
-      stub_request(:head, "http://example.com/404.html").
+      stub_request(:head, url).
         with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
         to_return(:status => 404, :body => "", :headers => {})
-      u.check_remote_uri("http://example.com/404.html", "test.html")
+      u.check_remote_uri(url, html_file)
       puts u.errors.inspect
-      u.errors["test.html"].should_not be_empty
-      u.errors["test.html"]["http://example.com/404.html"].instance_of?(Net::HTTPNotFound).should be_true
+      u.errors[html_file].should_not be_empty
+      u.errors[html_file][url].instance_of?(Net::HTTPNotFound).should be_true
     end
 
     it "should add error status from buffer timeouts" do
-      stub_request(:head, "http://example.com/index.html").to_timeout
-      u.check_remote_uri("http://example.com/index.html", "test.html")
+      stub_request(:head, url).to_timeout
+      u.check_remote_uri(url, html_file)
       u.errors.should_not be_empty
     end
 
     it "should add error status from connection timeouts" do
-      stub_request(:head, "http://example.com/index.html").to_raise(Errno::ETIMEDOUT)
-      u.check_remote_uri("http://example.com/index.html", "test.html")
+      stub_request(:head, url).to_raise(Errno::ETIMEDOUT)
+      u.check_remote_uri(url, html_file)
       u.errors.should_not be_empty
     end
 
     it "shoud add error status from name resolution errors" do
-      stub_request(:head, "http://example.com/index.html").
+      stub_request(:head, url).
         to_raise(SocketError.new('getaddrinfo: Name or service not known'))
-      u.check_remote_uri("http://example.com/index.html", "test.html")
+      u.check_remote_uri(url, html_file)
       u.errors.should_not be_empty
     end
 
     it "shoud add error status when invalid URI" do
       URI.stub(:new).and_raise(URI::InvalidURIError)
-      u.check_remote_uri("http://invalid_uri", "test.html")
+      u.check_remote_uri("http://invalid_uri", html_file)
       u.errors.should_not be_empty
     end
   end
